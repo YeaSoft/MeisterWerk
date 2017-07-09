fde67d7fb52e77566605ee57afb6b32259f0b8c8
Leo Moll <leo.moll@dtms.de>
2017-07-09 22:26:44 +0200

    Intermediate commit

--- core/entity.h
+++ core/entity.h
@@ -13,7 +13,10 @@
 #define MW_STATE_ON 1
 #define MW_STATE_UNDEFINED 2
 
+// entity configuration
+#ifndef MW_MSG_REG_MAXNAME
 #define MW_MSG_REG_MAXNAME 32
+#endif
 
 #include "helpers.h"
 #include "message.h"
@@ -47,16 +50,17 @@ namespace meisterwerk {
             // This sends messages to scheduler via messageQueue
             bool sendMessage( unsigned int type, String topic, char *pBuf, unsigned int len,
                               bool isBufAllocated = false ) {
-                DBG( "begin sendMessage, from: " + entName + ", topic: " + topic );
+                DBG( "entity::sendMessage, from: " + entName + ", topic: " + topic );
                 return message::sendMessage( type, entName, topic, pBuf, len, isBufAllocated );
             }
 
             // Send text message to Scheduler
             bool sendMessage( unsigned int type, String topic, String content ) {
-                DBG( "begin sendMessage, from: " + entName + ", topic: " + topic );
+                DBG( "entity::sendMessage, from: " + entName + ", topic: " + topic );
                 return message::sendMessage( type, entName, topic, content );
             }
 
+            public:
             virtual ~entity(){}; // Otherwise destructor of derived classes is never
                                  // called!
 
@@ -65,7 +69,7 @@ namespace meisterwerk {
                                  unsigned int priority = 1 ) {
                 T_MW_MSG_REGISTER mr;
                 if ( eName.length() >= MW_MSG_REG_MAXNAME - 1 ) {
-                    Serial.println( "Name to long for registration: " + eName );
+                    DBG( "entity::registerEntity, Name to long for registration: " + eName );
                     return false;
                 }
                 memset( &mr, 0, sizeof( mr ) );
@@ -77,8 +81,9 @@ namespace meisterwerk {
                 mr.priority     = priority;
                 bool ret =
                     sendMessage( message::MSG_DIRECT, "register", (char *)&mr, sizeof( mr ) );
-                if ( !ret )
-                    Serial.println( "sendMessage failed for register " + eName );
+                if ( !ret ) {
+                    DBG( "entity::registerEntity, sendMessage failed for register " + eName );
+                }
                 return ret;
             }
 
@@ -91,16 +96,14 @@ namespace meisterwerk {
                 return false;
             }
 
-            virtual void loop( unsigned long ticker ) {
-                Serial.println( "Loop: class for " + entName +
-                                " doesnt implement override! Wrong instance!" );
-                return;
+            virtual void onLoop( unsigned long ticker ) {
+                DBG( "entity:onLook, class for " + entName +
+                     " doesnt implement override! Wrong instance!" );
             }
 
-            virtual void receiveMessage( String topic, char *pBuf, unsigned int len ) {
-                Serial.println( "receiveMessage: class for " + entName +
-                                " doesnt implement override! Wrong instance!" );
-                return;
+            virtual void onReceiveMessage( String topic, char *pBuf, unsigned int len ) {
+                DBG( "entity:receiveMessage, class for " + entName +
+                     " doesnt implement override! Wrong instance!" );
             }
 
             // members
