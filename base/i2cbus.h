8b69a7bc631a5d5aee4cf0e5d7f65846dc8869ab
Dominik Schloesser <dsc@dosc.net>
2017-07-11 20:14:20 +0200

    std::vector eliminated.

--- base/i2cbus.h
+++ base/i2cbus.h
@@ -62,61 +62,61 @@ enum I2CDev {
 }
 
 typedef struct t_i2c_properties {
-    I2CDev                     id;
-    I2CDevType                 type;
-    String                     name;
-    String                     description;
-    bool                       supported;
-    std::vector<unsigned char> addresses;
-} T_I2C_PROPERTIES
+    I2CDev        id;
+    I2CDevType    type;
+    String        name;
+    String        description;
+    bool          supported;
+    unsigned char addresses{};
+} T_I2C_PROPERTIES;
 
-    const T_I2C_PROPERTIES i2cProps[] PROGMEM = {
-        {OLED, SSD1306, "SSD1306", "OLED-display (128x64)", false, {0x3C, 0x3D}},
-        // http://www.solomon-systech.com/en/product/display-ic/oled-driver-controller/ssd1306/
-        {NFC, PN532, "PN532", "RFID controller", false, {0x48}},
-        // https://www.adafruit.com/product/364
-        {Sensor, TSL2561, "TSL2561", "Luminosity sensor", false, {0x29, 0x39, 0x49}},
-        // https://www.adafruit.com/product/439
-        {Sensor, BMP085, "BMP085", "Pressure, temperature, altitude Sensor", false, {0x77}},
-        // https://www.adafruit.com/product/391
-        {Sensor, ADXL345, "ADXL345", "Accelerometer", false, {0x1D, 0x53}},
-        // http://www.analog.com/en/products/mems/accelerometers/adxl345.html
-        {Sensor, HMC5883L, "HMC5883L", "Magnetic compass", false, {0x1E}},
-        // https://aerospace.honeywell.com/en/products/navigation-and-sensors/magnetic-sensors-and-transducers
-        {Sensor, BMA180, "BMA180", "3 axis accelerometer", false, {0x77}},
-        // http://www.geeetech.com/wiki/index.php/BMA180_Triple_Axis_Accelerometer_Breakout
-        {Sensor, VCNL4000, "VCNL4000", "Proximity light sensor", false, {0x13}},
-        // https://www.adafruit.com/product/466
-        {Sensor, ITG3200, "ITG3200", "3 axis accelerometer", false, {0x68, 0x69}},
-        // https://www.invensense.com/products/motion-tracking/3-axis/itg-3200/
-        {Sensor, MS5607, "MS5607", "Altimeter", false, {0x76, 0x77}},
-        // https://www.parallax.com/product/29124
-        {Sensor, MS5611, "MS5611", "Barometric pressure altitude sensor", false, {0x76, 0x77}},
-        // http://www.amsys.info/products/ms5611.htm
-        {Sensor, LSM9DS0_Accel_Mag, "LSM9DS0_Accel_Mag", "", false, {0x1D}},
-        {Sensor, LSM9DS0_Gyro, "LSM9DS0_Gyro", "", false, {0x6B}},
-        {Sensor, LSM303_Accel, "LSM303_Accel", "", false, {0x19}},
-        {Sensor, LSM303_Mag, "LSM303_Mag", "", false, {0x1E}},
-        {RTC, DS1307, "DS1307", "", false, {0x68}},
-        {RTC, DS3231, "DS3231", "", false, {0x68}},
-        {EEPROM, DS1307_EEPROM, "DS1307_EEPORM", "", false, {0x50}},
-        {IO, MCP23008, "MCP23008", "", false, {0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27}},
-        {IO, MCP23017, "MCP23017", "", false, {0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27}},
-        {PWM,
-         PCA9685,
-         "PCA9685",
-         "",
-         false,
-         {0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E,
-          0x4F}},
-        {DAC, MCP4725A0, "MCP4725A0", "", false, {0x60, 0x61}},
-        {DAC, MCP4725A1, "MCP4725A1", "", false, {0x62, 0x63}},
-        {DAC, MCP4725A2, "MCP4725A2", "", false, {0x64, 0x65}},
-        {DAC, MCP4725A3, "MCP4725A3", "", false, {0x66, 0x67}},
-        {Radio, TEA5767, "TEA5767", "", false, {0x60}},
-        {Radio, Si4713, "Si4713", "", false, {0x11, 0x63}},
-        {Touch, FT6206, "FT6206", "", false, {0x38}},
-        {Touch, STMPE610, "STMPE610", "", false, {0x41}}};
+const T_I2C_PROPERTIES i2cProps[] PROGMEM = {
+    {OLED, SSD1306, "SSD1306", "OLED-display (128x64)", false, {0x3C, 0x3D}},
+    // http://www.solomon-systech.com/en/product/display-ic/oled-driver-controller/ssd1306/
+    {NFC, PN532, "PN532", "RFID controller", false, {0x48}},
+    // https://www.adafruit.com/product/364
+    {Sensor, TSL2561, "TSL2561", "Luminosity sensor", false, {0x29, 0x39, 0x49}},
+    // https://www.adafruit.com/product/439
+    {Sensor, BMP085, "BMP085", "Pressure, temperature, altitude Sensor", false, {0x77}},
+    // https://www.adafruit.com/product/391
+    {Sensor, ADXL345, "ADXL345", "Accelerometer", false, {0x1D, 0x53}},
+    // http://www.analog.com/en/products/mems/accelerometers/adxl345.html
+    {Sensor, HMC5883L, "HMC5883L", "Magnetic compass", false, {0x1E}},
+    // https://aerospace.honeywell.com/en/products/navigation-and-sensors/magnetic-sensors-and-transducers
+    {Sensor, BMA180, "BMA180", "3 axis accelerometer", false, {0x77}},
+    // http://www.geeetech.com/wiki/index.php/BMA180_Triple_Axis_Accelerometer_Breakout
+    {Sensor, VCNL4000, "VCNL4000", "Proximity light sensor", false, {0x13}},
+    // https://www.adafruit.com/product/466
+    {Sensor, ITG3200, "ITG3200", "3 axis accelerometer", false, {0x68, 0x69}},
+    // https://www.invensense.com/products/motion-tracking/3-axis/itg-3200/
+    {Sensor, MS5607, "MS5607", "Altimeter", false, {0x76, 0x77}},
+    // https://www.parallax.com/product/29124
+    {Sensor, MS5611, "MS5611", "Barometric pressure altitude sensor", false, {0x76, 0x77}},
+    // http://www.amsys.info/products/ms5611.htm
+    {Sensor, LSM9DS0_Accel_Mag, "LSM9DS0_Accel_Mag", "", false, {0x1D}},
+    {Sensor, LSM9DS0_Gyro, "LSM9DS0_Gyro", "", false, {0x6B}},
+    {Sensor, LSM303_Accel, "LSM303_Accel", "", false, {0x19}},
+    {Sensor, LSM303_Mag, "LSM303_Mag", "", false, {0x1E}},
+    {RTC, DS1307, "DS1307", "", false, {0x68}},
+    {RTC, DS3231, "DS3231", "", false, {0x68}},
+    {EEPROM, DS1307_EEPROM, "DS1307_EEPORM", "", false, {0x50}},
+    {IO, MCP23008, "MCP23008", "", false, {0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27}},
+    {IO, MCP23017, "MCP23017", "", false, {0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27}},
+    {PWM,
+     PCA9685,
+     "PCA9685",
+     "",
+     false,
+     {0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E,
+      0x4F}},
+    {DAC, MCP4725A0, "MCP4725A0", "", false, {0x60, 0x61}},
+    {DAC, MCP4725A1, "MCP4725A1", "", false, {0x62, 0x63}},
+    {DAC, MCP4725A2, "MCP4725A2", "", false, {0x64, 0x65}},
+    {DAC, MCP4725A3, "MCP4725A3", "", false, {0x66, 0x67}},
+    {Radio, TEA5767, "TEA5767", "", false, {0x60}},
+    {Radio, Si4713, "Si4713", "", false, {0x11, 0x63}},
+    {Touch, FT6206, "FT6206", "", false, {0x38}},
+    {Touch, STMPE610, "STMPE610", "", false, {0x41}}};
 
 namespace meisterwerk {
     namespace base {
@@ -125,7 +125,7 @@ namespace meisterwerk {
             bool         bSetup;
             bool         bEnum;
             bool         bInternalError;
-            uint8_5      sdaport, sclport;
+            uint8_t      sdaport, sclport;
             unsigned int nDevices;
 
             i2cbus( String name, int sdaport, int sclport )
