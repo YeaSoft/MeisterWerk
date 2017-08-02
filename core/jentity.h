acd3ea9084f36b1d14f49ec736d4a24b329dcaeb
Leo Moll <leo.moll@dtms.de>
2017-08-02 18:46:53 +0200

    Integrated i2c support into jentity

--- core/jentity.h
+++ core/jentity.h
@@ -17,6 +17,7 @@
 #include <ArduinoJson.h>
 
 // dependencies
+#include "../util/hextools.h"
 #include "../util/msgtime.h"
 #include "array.h"
 #include "entity.h"
@@ -42,19 +43,81 @@ namespace meisterwerk {
                 }
             };
 
-            array<controlword> wordList;
-
-            public:
+            protected:
             // members
-            bool bValidTime = false;
+            array<controlword> wordList;
+            String             i2ctype;
+            uint8_t            address;
+            bool               isInstantiated = false;
+            bool               bValidTime     = false;
 
             public:
+            // Constructor for non i2c entities
             jentity( String name, unsigned long minMicroSecs, T_PRIO priority = PRIORITY_NORMAL,
                      unsigned int wordListSize = 16 )
                 : entity( name, minMicroSecs, priority ), wordList( wordListSize ) {
-                subscribe( "info" );
-                subscribe( "mastertime/time/set" );
-                registerWord( "info", controlword::READ );
+                commonInit();
+            }
+
+            // Constructor for i2c entities
+            jentity( String name, String i2cType, uint8_t address, unsigned int wordListSize = 16 )
+                : meisterwerk::core::entity( name, 50000 ),
+                  wordList( wordListSize ), i2ctype{i2cType}, address{address} {
+                // 50 ms bus poll interval
+                DBG( "i2ctype:" + i2ctype );
+                commonInit();
+            }
+
+            // entity overridables
+            virtual void setup() override {
+                if ( i2ctype.length() > 0 ) {
+                    // i2c entity
+                    subscribe( "i2cbus/devices" );
+                    entity::publish( "i2cbus/devices/get" );
+                }
+            }
+
+            virtual void receive( const char *origin, const char *topic, const char *msg ) override {
+                DynamicJsonBuffer reqBuffer( 256 );
+                DynamicJsonBuffer resBuffer( 256 );
+                JsonObject &      req = reqBuffer.parseObject( msg );
+                JsonObject &      res = resBuffer.createObject();
+                Topic             tpc = topic;
+
+                prepareData( res );
+
+                if ( tpc == "i2cbus/devices" && i2ctype.length() > 0 ) {
+                    if ( !isInstantiated ) {
+                        i2cSetup( req, res );
+                    }
+                } else if ( tpc == "mastertime/time/set" ) {
+                    bValidTime = true;
+                } else if ( tpc == "info" || tpc == entName + "/info/get" ) {
+                    prepareInfo( res );
+                    onGetValue( "info", req, res );
+                } else if ( tpc.match( entName + "/+" ) ) {
+                    onReaction( tpc.gettail(), req, res );
+                } else if ( tpc.match( entName + "/+/get" ) ) {
+                    onGetValue( tpc.gettail().getfirst(), req, res );
+                } else if ( tpc.match( entName + "/+/set" ) ) {
+                    onSetValue( tpc.gettail().getfirst(), req, res );
+                } else {
+                    onMessage( topic, req, res );
+                }
+            }
+
+            // jentity overridables
+            virtual void onInstantiate( String i2ctype, uint8_t address ) {
+                DBG( "Your code should override this function and instantiate a " + i2ctype + " device at address 0x" +
+                     meisterwerk::util::hexByte( address ) );
+            }
+            virtual void onReaction( String cmd, JsonObject &params, JsonObject &data ) {
+            }
+            virtual void onGetValue( String value, JsonObject &params, JsonObject &data ) {
+            }
+            virtual void onSetValue( String value, JsonObject &params, JsonObject &data ) {
+            }
+            virtual void onMessage( Topic topic, JsonObject &params, JsonObject &data ) {
             }
 
             bool notify( const char *name, JsonObject &json ) const {
@@ -81,7 +144,7 @@ namespace meisterwerk {
                 return entity::publish( topic.c_str(), buffer.c_str() );
             }
 
-            void Reaction( const char *wordName ) {
+            void Reaction( const char *wordName, bool bGeneric = false ) {
                 registerWord( wordName, controlword::REACT );
             }
 
@@ -162,38 +225,31 @@ namespace meisterwerk {
                 return topic == entName + "/" + subtopic;
             }
 
-            virtual void receive( const char *origin, const char *topic, const char *msg ) override {
-                DynamicJsonBuffer reqBuffer( 256 );
-                DynamicJsonBuffer resBuffer( 256 );
-                JsonObject &      req = reqBuffer.parseObject( msg );
-                JsonObject &      res = resBuffer.createObject();
-                Topic             tpc = topic;
+            // internal helpers
+            protected:
+            void commonInit() {
+                subscribe( "info" );
+                subscribe( "mastertime/time/set" );
+                registerWord( "info", controlword::READ );
+            }
 
-                prepareData( res );
-
-                if ( tpc == "mastertime/time/set" ) {
-                    bValidTime = true;
-                } else if ( tpc == "info" || tpc == entName + "/info/get" ) {
-                    prepareInfo( res );
-                    onGetValue( "info", req, res );
-                } else if ( tpc.match( entName + "/+" ) ) {
-                    onReaction( tpc.gettail(), req, res );
-                } else if ( tpc.match( entName + "/+/get" ) ) {
-                    onGetValue( tpc.gettail().getfirst(), req, res );
-                } else if ( tpc.match( entName + "/+/set" ) ) {
-                    onSetValue( tpc.gettail().getfirst(), req, res );
-                } else {
-                    onMessage( topic, req, res );
+            void i2cSetup( JsonObject &params, JsonObject &data ) {
+                if ( !params.success() ) {
+                    DBG( "jentity::i2cSetup - Invalid JSON received" );
+                    return;
                 }
-            }
-
-            virtual void onReaction( String cmd, JsonObject &params, JsonObject &data ) {
-            }
-            virtual void onGetValue( String value, JsonObject &params, JsonObject &data ) {
-            }
-            virtual void onSetValue( String value, JsonObject &params, JsonObject &data ) {
-            }
-            virtual void onMessage( Topic topic, JsonObject &params, JsonObject &data ) {
+                JsonArray &devs = params["devices"];
+                for ( int i = 0; i < devs.size(); i++ ) {
+                    JsonObject &dev = devs[i];
+                    for ( auto obj : dev ) {
+                        DBG( String( obj.key ) + "<->" + i2ctype );
+                        if ( i2ctype == obj.key && address == obj.value ) {
+                            onInstantiate( i2ctype, address );
+                            isInstantiated = true;
+                            return;
+                        }
+                    }
+                }
             }
         };
 
